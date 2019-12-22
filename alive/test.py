9baef135673e19f276de7b88ba557cedb2725c46
lmckiwo <lmckiwo@gmail.com>
2019-12-21 22:48:04 -0500

    updated test case

--- alive/test.py
+++ alive/test.py
@@ -402,8 +402,8 @@ class TestATICHosts(unittest.TestCase):
         self.assertEqual(self.atic.status.readStatus(self.testHost2, self.node2, "u_connected"), self.atic.CONNECTED)
         self.assertTrue(self.atic.disconnectHost(self.testHost2, self.node2))
         self.assertEqual(self.atic.status.readStatus(self.testHost2, self.node2, "u_connected"), self.atic.NOTCONNECTED)
-         self.assertTrue(self.atic.disconnectHost(self.testHost2, self.node2))
-         self.assertEqual(self.atic.status.readStatus(self.testHost2, self.node2, "u_connected"), self.atic.NOTCONNECTED)
+        self.assertTrue(self.atic.disconnectHost(self.testHost2, self.node2))
+        self.assertEqual(self.atic.status.readStatus(self.testHost2, self.node2, "u_connected"), self.atic.NOTCONNECTED)
 
     def test_isRemoteIndexExists(self):
         self.assertEqual(self.atic._connect(self.testHost2, self.node2, updateStatus=True), self.atic.CONNECTED)
